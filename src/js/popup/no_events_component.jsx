7dba05d1ffe6ce310ed9d5ef5e7aef7d30fa1beb
melissaroman <melroman26@gmail.com>
2017-05-24 11:35:49 -0700

    Adds no event component

---
+++ src/js/popup/no_events_component.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class NoEvents extends Component {
+  render() {
+    return (
+      <div>No events found for artist.</div>
+    )
+  }
+}
