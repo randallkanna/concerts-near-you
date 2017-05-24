bc745c6c5e7af848790788ae2af111f6d5c0ec28
melissaroman <melroman26@gmail.com>
2017-05-24 16:36:53 -0700

    adds styles to no events component

--- src/js/popup/no_events_component.jsx
+++ src/js/popup/no_events_component.jsx
@@ -1,5 +1,17 @@
 import React from 'react';
 
-const NoEvents = () => (<div>No events found for artist.</div>);
+const noEventStyle = {
+  width: '336px',
+  height: 'auto',
+  padding: '34px 0 43px',
+  textAlign: 'center',
+  verticalAlign: 'middle',
+  fontSize: '15px',
+  fontFamily: 'Avenir',
+  fontWeight: 'medium',
+  color: '#585858'
+};
+
+const NoEvents = () => (<div style={noEventStyle}>No events found for artist.</div>);
 
 export default NoEvents;
